0ace8e4179d5006f56a4c5f39662a2e66dbc71b2
Satoshi Tanaka in U <st34.satoshi@gmail.com>
2020-02-03 13:26:54 +0900

    created enc_state

---
+++ enc_state.cpp
@@ -0,0 +1,53 @@
+/*
+encoding
+i <--> state
+*/
+
+// nCr, vector of the state. if o or x exist 1, else o
+// r <= n
+// example. n=9, r=4, i=0,  StateArrayFromI[n][r][i] = {0,0,0,0,0,1,1,1,1}
+//                    i=1,                             {0,0,0,0,1,0,1,1,1}
+array<array<vector<vector<bool>>, combinationSize+1>, combinationSize+1> StateArrayFromI;
+// example. n=9, r=4, s(state only of x)=000001111,  IndexArrayFromState[n][r][s] = 0
+//                    s(state only of x)=000010111,                               = 1
+array<array<vector<ll>, combinationSize+1>, combinationSize+1> IndexArrayFromState;
+
+void initEncodingArray(bool selectedStates[], int selectedStateSize, int markNumber, int remain, ll* ind, int selectedMaxI){
+    if (remain==0){
+        // initialize StateArrayFromI and IndexArrayFromState
+        for(int i=0;i<selectedStateSize;i++){
+            StateArrayFromI[selectedStateSize][markNumber].at(*ind).at(i) = selectedStates[i];
+        }
+        ll s = 0ll;
+        for(int i=0;i<selectedStateSize;i++){
+            if(selectedStates[i]){
+                s += 1ll << (selectedStateSize - 1 - i);
+            }
+        }
+        IndexArrayFromState[selectedStateSize][markNumber].at(s) = *ind;
+        *ind += 1;
+        return;
+    }
+    for(int i=selectedMaxI+1;i<=selectedStateSize-remain;i++){
+        selectedStates[i] = true;
+        initEncodingArray(selectedStates, selectedStateSize, markNumber, remain - 1, ind, i);
+        selectedStates[i] = false;
+    }
+}
+
+void initEncoding(){
+    for(int i=1;i<combinationSize;i++){
+        for(int j=1;j<=i;j++){
+            // resize the vector
+            vector<vector<bool>> w(combinations[i][j], vector<bool>(i));
+            StateArrayFromI[i][j] = w;
+            vector<ll> v(1ll << i);
+            IndexArrayFromState[i][j] = v;
+
+            ll ind = 0ll;
+            bool selectedStates[i];
+            for(int k=0;k<i;k++){ selectedStates[k]=false; }
+            initEncodingArray(selectedStates, i, j, j, &ind, -1);
+        }
+    }
+}
