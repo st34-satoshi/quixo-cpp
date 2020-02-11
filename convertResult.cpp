f71be150653c4de4b480b897a2032f3535252b85
Satoshi Tanaka in U <st34.satoshi@gmail.com>
2020-02-11 19:27:04 +0900

    tmp

--- convertResult.cpp
+++ convertResult.cpp
@@ -3,7 +3,8 @@
 void convertResultsTOXO(){
     int countState = 0;
     unsigned int win=0, draw=0, loss=0;
-    for(int total=combinationSize; total>=0 ;total--){
+    // for(int total=combinationSize; total>=0 ;total--){
+    for(int total=combinationSize-15; total>=0 ;total--){
         cout << "total = " << total << endl;
         for(int oNumber=0;oNumber<=total;oNumber++){
             int xNumber = total - oNumber;
@@ -11,9 +12,9 @@ void convertResultsTOXO(){
             readStatesValue(&values, oNumber, xNumber);
             // TODO save to file
             for(ull i=0; i<values.size()/2ll;i++){
-                // ll s = generateState(i, oNumber, xNumber);
-                // cout << "value = " << values.at(i*2) << values.at(i*2+1) << endl;
-                // printState(s);
+                ll s = generateState(i, oNumber, xNumber);
+                cout << "value = " << values.at(i*2) << values.at(i*2+1) << endl;
+                printState(s);
                 countState++;
                 if (!values.at(i*2ll)&&values.at(i*2ll+1ll)) win++;
                 else if (values.at(i*2ll+1ll) && values.at(i*2ll+1ll)) loss++;
@@ -50,61 +51,72 @@ void outputStepResult(){
     vector<int> winSteps(maxStep);
     vector<int> lossSteps(maxStep);
     int step;
-    for(int total=combinationSize; total>=0 ;total--){
-    // for(int total=combinationSize; total>=16 ;total--){
+    // for(int total=combinationSize; total>=0 ;total--){
+    for(int total=3; total>=3 ;total--){
         cout << "total = " << total << endl;
-        for(int oNumber=0;oNumber<=total;oNumber++){
-        // for(int oNumber=8;oNumber<=8;oNumber++){
+        // for(int oNumber=0;oNumber<=total;oNumber++){
+        for(int oNumber=1;oNumber<=1;oNumber++){
             int xNumber = total - oNumber;
             vector<int> statesStep(combinations[combinationSize][oNumber] * combinations[(combinationSize-oNumber)][xNumber]);
             readStatesStep(&statesStep, oNumber, xNumber);
             vector<bool> values(combinations[combinationSize][oNumber] * combinations[(combinationSize-oNumber)][xNumber] * 2);
             readStatesValue(&values, oNumber, xNumber);
+            vector<bool> statesReachable(combinations[combinationSize][oNumber] * combinations[(combinationSize-oNumber)][xNumber] * 2ll);
+            readStatesReachable(&statesReachable, oNumber, xNumber);
             // TODO save to file
+            int dC = 0, wC = 0, lC = 0;
             for(ull i=0ull;i<statesStep.size();i++){
+                if(!statesReachable[i*2ll]) continue;
                 // TODO: if you want to see the result of state(win, loss or draw and step), please remove the "//"
-                // ll s = generateState(i, oNumber, xNumber);
+                ll s = generateState(i, oNumber, xNumber);
                 step = statesStep.at(i);
                 // cout << "step = " << step << endl;
                 if(!values.at(i*2ll+1ll)){
                     //  cout << "v = draw" << endl; 
                      draw++;
+                     dC++;
+                    //  printState(s);
                 }else if(values.at(i*2ll)){
                     //  cout << "v = loss" << endl; 
                      loss++;
                      lossSteps[step]++;
+                     lC++;
+                     printState(s);
                 }else{
                     // cout << "v = win" << endl; 
                     win++;
                     winSteps[step]++;
+                    wC++;
+                    printState(s);
                 }
                 // printState(s);
                 all++;
             }
+            cout << "o, x, d, w, l = " << oNumber << ", " << xNumber << ", " << dC << ", " << wC << ", " << lC << endl;
         }
     }
-    cout << "total states = " << all << endl;
-    cout << "win states = " << win << endl;
-    cout << "loss states = " << loss << endl;
-    cout << "draw states = " << draw << endl;
-    cout << endl;
-    cout << "steps to win" << endl;
-    for(int i=0;i<maxStep;i++){
-        cout << "step=" << i << ", the number of states = " << winSteps[i] << endl;
-    }
-    cout << endl;
-    cout << "steps to loss" << endl;
-    for(int i=0;i<maxStep;i++){
-        cout << "step=" << i << ", the number of states = " << lossSteps[i] << endl;
-    }
-    cout << endl;
+    // cout << "total states = " << all << endl;
+    // cout << "win states = " << win << endl;
+    // cout << "loss states = " << loss << endl;
+    // cout << "draw states = " << draw << endl;
+    // cout << endl;
+    // cout << "steps to win" << endl;
+    // for(int i=0;i<maxStep;i++){
+    //     cout << "step=" << i << ", the number of states = " << winSteps[i] << endl;
+    // }
+    // cout << endl;
+    // cout << "steps to loss" << endl;
+    // for(int i=0;i<maxStep;i++){
+    //     cout << "step=" << i << ", the number of states = " << lossSteps[i] << endl;
+    // }
+    // cout << endl;
     // cout << "total " << endl;
     // int t = 0;
     // for(int i=0;i<maxStep;i++){
     //     t += lossSteps[i] + winSteps[i];
     //     cout << i << " ; " << t << endl;
     // }
-    printInitialStateResult();
+    // printInitialStateResult();
 }
 
 void outputReachableStates(){
