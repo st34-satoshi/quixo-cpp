fa16da65a24b5b7455b0bf927fb6b13825f6a69b
Satoshi Tanaka in U <st34.satoshi@gmail.com>
2020-03-11 18:33:43 +0900

    added cout the total win, loss, draw by python

--- convertResult.cpp
+++ convertResult.cpp
@@ -6,7 +6,7 @@ void convertResultsTOXO(){
     for(int total=combinationSize; total>=0 ;total--){
         // cout << "total = " << total << endl;
         for(int oNumber=0;oNumber<=total;oNumber++){
-            unsigned int win=0, draw=0, loss=0;
+            unsigned long long win=0, draw=0, loss=0;
             int xNumber = total - oNumber;
             vector<bool> values(combinations[combinationSize][oNumber] * combinations[(combinationSize-oNumber)][xNumber] * 2);
             readStatesValue(&values, oNumber, xNumber);
---
+++ python/count_total.py
@@ -0,0 +1,68 @@
+from scipy.special import comb
+
+
+def read_file(file_name):
+    total_win = 0
+    total_loss = 0
+    total_draw = 0
+    # read each line
+    print("read each line")
+    with open(file_name) as f:
+        data_list = f.readlines()
+        p_win = 0
+        p_loss = 0
+        p_draw = 0
+        co = 0
+        for i, data in enumerate(data_list):
+            print(data)
+
+            if i % 6 == 1:
+                x, o = xo(data)
+                print("x,o = ", x, o)
+                p_win = 0
+                p_loss = 0
+                p_draw = 0
+                print("comb = ", comb(25, x), comb(25-x, o))
+                co = comb(25, x) * comb(25 - x, o)
+                print("co = ", co)
+            if i % 6 in [2, 3, 4]:
+                numb = deal_line(data)
+                print(numb)
+                print("i=", i)
+                if i % 6 == 2:
+                    p_win += numb
+                    # print(total_win)
+                elif i % 6 == 3:
+                    p_loss += numb
+                else:
+                    p_draw += numb
+
+            if i % 6 == 5:
+                total_win += p_win
+                total_loss += p_loss
+                total_draw += p_draw
+                print("present ", p_win, p_loss, p_draw)
+                if co != (p_draw+p_loss+p_win):
+                    print("Error")
+    print("total win = ", total_win)
+    print("total loss = ", total_loss)
+    print("total draw = ", total_draw)
+    print(total_win+total_loss+total_draw)
+
+
+def deal_line(l):
+    l_list = l.split("= ")
+    n = l_list[1]
+    return int(n)
+
+
+def xo(l):
+    l_list = l.split(",")
+    x = l_list[1].split("= ")[1]
+    o = l_list[2].split("= ")[1]
+    return int(x), int(o)
+
+
+if __name__ == '__main__':
+    f = "result-5by5-activeIsO.txt"
+    read_file(f)
