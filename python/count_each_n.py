4453c27100e93ea7f556da5474d7980b81e0d8c4
Satoshi Tanaka in U <st34.satoshi@gmail.com>
2020-03-16 16:40:06 +0900

    able to make figure of each n % or Win, Loss, Draw states number

---
+++ python/count_each_n.py
@@ -0,0 +1,105 @@
+from scipy.special import comb
+import matplotlib.pyplot as plt
+
+
+def read_file(file_name):
+    # o number, x number, W,L,D
+    ox_values = [[[0 for _ in range(3)] for _ in range(25+1)] for _ in range(25+1)]
+    total_win = 0
+    total_loss = 0
+    total_draw = 0
+    # read each line
+    print("read each line")
+    with open(file_name) as f:
+        data_list = f.readlines()
+        p_win = 0
+        p_loss = 0
+        p_draw = 0
+        co = 0
+        for i, data in enumerate(data_list):
+            if i % 6 == 1:
+                x, o = xo(data)
+                p_win = 0
+                p_loss = 0
+                p_draw = 0
+                co = comb(25, x) * comb(25 - x, o)
+            if i % 6 in [2, 3, 4]:
+                numb = deal_line(data)
+                if i % 6 == 2:
+                    p_win += numb
+                    # print(total_win)
+                elif i % 6 == 3:
+                    p_loss += numb
+                else:
+                    p_draw += numb
+
+            if i % 6 == 5:
+                total_win += p_win
+                total_loss += p_loss
+                total_draw += p_draw
+                if co != (p_draw+p_loss+p_win):
+                    print("Error")
+                    print("o = ", o, ", x=", x)
+                    exit()
+                ox_values[o][x][0] = p_win
+                ox_values[o][x][1] = p_loss
+                ox_values[o][x][2] = p_draw
+    print("total win = ", total_win)
+    print("total loss = ", total_loss)
+    print("total draw = ", total_draw)
+    print(total_win+total_loss+total_draw)
+    return ox_values
+
+
+def deal_line(l):
+    l_list = l.split("= ")
+    n = l_list[1]
+    return int(n)
+
+
+def xo(l):
+    l_list = l.split(",")
+    x = l_list[1].split("= ")[1]
+    o = l_list[2].split("= ")[1]
+    return int(x), int(o)
+
+
+def each_n(ox_values):
+    total_values = [[0 for _ in range(4)] for _ in range(25+1)]  # w,l,d, total
+    for n in range(25+1):
+        for o in range(n+1):
+            x = n - o
+            for i in range(3):
+                total_values[n][i] += ox_values[o][x][i]
+        total = 0
+        for i in range(3):
+            total += total_values[n][i]
+        total_values[n][3] = total
+        # print("///////////// total = ", n)
+        # print("win = ", total_values[n][0] / total)
+        # print("loss = ", total_values[n][1] / total)
+        # print("draw = ", total_values[n][2] / total)
+    return total_values
+
+
+def plot_WLD_per_total(total_values):
+    n = [i for i in range(25+1)]
+    win = [total_values[i][0] / total_values[i][3] for i in range(25+1)]
+    loss = [total_values[i][1] / total_values[i][3] for i in range(25+1)]
+    draw = [total_values[i][2] / total_values[i][3] for i in range(25+1)]
+    plt.plot(n, win, label="Win")
+    plt.plot(n, loss, label="Loss")
+    plt.plot(n, draw, label="Draw")
+    plt.ylabel("% of Win/Loss/Draw states number")
+    plt.xlabel("the number of o + x tiles")
+    plt.legend()
+    plt.savefig("WLDPerN")
+    # plt.show()
+
+
+if __name__ == '__main__':
+    f = "result-5by5-activeIsO.txt"
+    ox_v = read_file(f)
+    # print(ox_v)
+    total_v = each_n(ox_v)
+    plot_WLD_per_total(total_v)
